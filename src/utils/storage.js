4b7ac50d7cafc39306c5867046fa4dcdbe31c951
Roman-Shashkov <romanshashkov87@gmail.com>
2020-12-18 15:53:00 +0300

    check second

--- src/utils/storage.js
+++ src/utils/storage.js
@@ -1,6 +1,5 @@
 
 const storage = {
-<<<<<<< HEAD
     set: ( k, v ) => {
         localStorage.setItem ( k, JSON.stringify(v))
     },
@@ -9,24 +8,10 @@ const storage = {
         try {
             v = JSON.parse (v);
         } catch (e) {
-=======
-    set: (k, v) => {
-        localStorage.setItem (k , JSON.stringify(v));
-    },
-    get: (k) => {
-        let v = localStorage.getItem (k);
-        try {
-           v = JSON.parse (v);
-        }   catch (e) {
->>>>>>> 28232b6f53f11223eb75b448231aa5bc6606b0f5
             v = '';
         }
         return v;
     },
 };
 
-<<<<<<< HEAD
 export default storage
-=======
-export default storage;
->>>>>>> 28232b6f53f11223eb75b448231aa5bc6606b0f5
